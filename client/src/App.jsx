1af0e0aee7d9db66f59ce2e21f1450c791fc0381
AnuOuseph <anuouseph04@gmail.com>
2023-09-11 17:07:16 +0530

    darkmode implemented

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,5 @@
 /* eslint-disable react/prop-types */
-import './app.css'
+import './app.scss'
 import {createBrowserRouter, Navigate, Outlet, RouterProvider} from 'react-router-dom'
 import Login from './pages/login/Login'
 import Signup from './pages/signup/Signup'
@@ -8,14 +8,18 @@ import Profile from './pages/profile/Profile'
 import NavBar from './components/navBar/NavBar'
 import LeftBar from './components/leftBar/LeftBar'
 import RightBar from './components/rightBar/RightBar'
+import { useContext } from 'react'
+import { DarkModeContext } from './context/DarkModeContext'
 
 function App() {
 
-  const currentUser = true
+  const currentUser = true;
+  const {darkMode} = useContext(DarkModeContext)
+  console.log(darkMode)
 
   const Layout = ()=>{
     return(
-      <div>
+      <div className={`theme-${darkMode ? "dark" : "light"}`}>
         <NavBar/>
         <div style={{display:'flex'}}>
           <LeftBar/>
--- client/src/components/navBar/NavBar.jsx
+++ client/src/components/navBar/NavBar.jsx
@@ -9,26 +9,29 @@ import NotificationsNoneOutlinedIcon from '@mui/icons-material/NotificationsNone
 import PersonOutlineOutlinedIcon from '@mui/icons-material/PersonOutlineOutlined';
 import SearchOutlinedIcon from '@mui/icons-material/SearchOutlined';
 import EmailOutlinedIcon from '@mui/icons-material/EmailOutlined';
+import { useContext } from "react";
+import { DarkModeContext } from "../../context/DarkModeContext";
 
 function NavBar() {
+  const {toggle,darkMode} = useContext(DarkModeContext)
   return (
     <div className="navBar">
       <div className="left">
         <Link to='/' style={{textDecoration: "none"}}>
           <span>Logo</span>
         </Link>
-        <HomeOutlinedIcon/>
-        <DarkModeOutlinedIcon/>
-        <GridViewOutlinedIcon/>
+        <HomeOutlinedIcon style={{width:"22px"}}/>
+        {darkMode?<WbSunnyOutlinedIcon style={{width:"22px"}} onClick={toggle}/>:<DarkModeOutlinedIcon style={{width:"22px"}} onClick={toggle}/>}
+        <GridViewOutlinedIcon style={{width:"22px"}}/>
         <div className="search">
-          <SearchOutlinedIcon/>
+          <SearchOutlinedIcon style={{width:"22px"}}/>
           <input type="text" placeholder="Search.." />
         </div>
       </div>
       <div className="right">
-        <PersonOutlineOutlinedIcon/>
-        <NotificationsNoneOutlinedIcon/>
-        <EmailOutlinedIcon/>
+        <PersonOutlineOutlinedIcon style={{width:"22px"}}/>
+        <NotificationsNoneOutlinedIcon style={{width:"22px"}}/>
+        <EmailOutlinedIcon style={{width:"22px"}}/>
         <div className="user">
           <img src="https://images.pexels.com/photos/3228727/pexels-photo-3228727.jpeg?auto=compress&cs=tinysrgb&w=1600" alt="" />
           <span>Jamie Doe</span>
